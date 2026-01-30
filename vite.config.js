200845124114fac85bc342e786e76a9a0b7825ee
steveliciouss <sfabriga@gbox.adnu.edu.ph>
2026-01-30 12:07:46 +0800

    Config: Add allowed host for Render

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    allowedHosts: ['weather-j92p.onrender.com'],
+  },
 })
 
